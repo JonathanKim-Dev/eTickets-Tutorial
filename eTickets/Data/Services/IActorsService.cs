7abee7bd49af66ff38ca0be53218bf85dafa311b
JonathanKim-Dev <JonathanKim.Dev@gmail.com>
2022-11-22 21:27:55 -0800

    Actors Dependency Injection

---
+++ eTickets/Data/Services/IActorsService.cs
@@ -0,0 +1,15 @@
+﻿using eTickets.Models;
+
+namespace eTickets.Data.Services
+{
+    public interface IActorsService
+    {
+        Task<IEnumerable<Actor>> GetAll();
+        Actor GetById(int id);
+        void Add(Actor actor);
+        Actor update(int id, Actor newActor);
+        void Delete(int id);
+
+
+    }
+}
