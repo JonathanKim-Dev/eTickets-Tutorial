164214f0f6c9231b824ae291a5f7c38c3eb6480d
JonathanKim-Dev <JonathanKim.Dev@gmail.com>
2022-11-28 16:03:11 -0800

    Create Actor Model Validation

--- eTickets/Models/Actor.cs
+++ eTickets/Models/Actor.cs
@@ -10,16 +10,22 @@ namespace eTickets.Models
 
         //In the site the ProfilePictureURL will appear as the string you put down. So if you didn't put "Profile Picture URL" it'd default as ProfilePictureURL
         [Display(Name = "Profile Picture")]
+        [Required(ErrorMessage = "Profile Picture is required")]
         public string ProfilePictureURL { get; set; }
 
         [Display(Name = "Full Name")]
+        [Required(ErrorMessage = "Full Name is required")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Full Name must be between 3 and 50 chars")]
         public string FullName { get; set; }
 
         [Display(Name = "Biography")]
+        [Required(ErrorMessage = "Biography is required")]
         public string Bio { get; set; }
 
         //Relationships
-        public List<Actor_Movie> Actors_Movies { get; set; }
+        //This has to be nullable because when you create actor you only input profile pic link, name, and bio.
+        //Actors_Movie remains empty while it is a parameter for the Actors model even though Actors_Movie is not a column in the data table.
+        public List<Actor_Movie>? Actors_Movies { get; set; }
 
       
 
